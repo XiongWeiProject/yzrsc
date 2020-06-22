3401c12c75450c96343518a31d56f5937e4be124
xiongwei <861739889@qq.com>
2020-06-22 18:41:34 +0800

    界面编写

---
+++ Android/Scw/app/src/main/java/com/commodity/yzrsc/ui/activity/friend/PayMoneNumActivity.java
@@ -0,0 +1,13 @@
+package com.commodity.yzrsc.ui.activity.friend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PayMoneNumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_mone_num);
+    }
+}
